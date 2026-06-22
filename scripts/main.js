9a2aab19f37f2a6002a7cca0f66c4bbdd7c7a0e9
okvolza <okvolzanisazaa@gmail.com>
2026-06-22 17:55:22 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,13 @@ Events.on(ClientLoadEvent, () => {
     if (itemBridge != null) {
         itemBridge.range = 100;
         itemBridge.itemCapacity = 100;
+        itemBridge.allOutputs = true; // 🌟 ปลดล็อกให้ปลายทางปล่อยไอเทมออกได้รอบตัว (4 ทิศ)
     }
 
     // สะพานของเหลว
     const liquidBridge = Vars.content.getByName(ContentType.block, "bridge-conduit");
     if (liquidBridge != null) {
-        liquidBridge.range = 100;        // ยาว 100 ช่อง
-        liquidBridge.liquidCapacity = 100; // จุของเหลว 100 หน่วย
+        liquidBridge.range = 100;
+        liquidBridge.liquidCapacity = 100;
     }
 });
